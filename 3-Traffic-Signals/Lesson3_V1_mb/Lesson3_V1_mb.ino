4f3afe7fafb643ffae7de1675d29fbb3b5531e84
Maxim Böckelmann <maxim.boeckelmann@mac.com>
2021-03-20 01:15:08 +0100

    Ester Test läuft

--- 3-Traffic-Signals/Lesson3_V1_mb/Lesson3_V1_mb.ino
+++ 3-Traffic-Signals/Lesson3_V1_mb/Lesson3_V1_mb.ino
@@ -1,9 +1,15 @@
 void setup() {
-  // put your setup code here, to run once:
+  // erst mal sagen welche Pins dran sind und was sie zu erwarten haben
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(3, HIGH); // stellt 5V an (=HIGH) auf Pin 3
+  digitalWrite(4, HIGH); // stellt 5V an auf Pin 4
+  digitalWrite(5, HIGH); // stellt 5V an auf Pin 5
 
 }
